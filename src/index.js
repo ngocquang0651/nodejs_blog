ac098e8ba85fb89c9cd26ec4e200b1494f94ee5a
ngocquang0651 <1712707@student.hcmus.edu.vn>
2022-08-18 00:04:44 +0700

    create folder controller and folder routes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // HTTP Logger
 app.use(morgan("combined"));
+app.use(express.static(__dirname + "/src/public"));
 app.engine(
   "hbs",
   engine({
@@ -19,12 +24,18 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
+app.set("search", path.join(__dirname, "resources/search"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/news", (req, res) => {
   res.render("news");
+});
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  res.render("search");
 });
 
 app.listen(3000, () => console.log("example app listening at ..."));
